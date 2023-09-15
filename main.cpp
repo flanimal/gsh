668215b9f29b7cdd7b731155734aed5a2c305c42
Nick Smith <nbsmithcc@gmail.com>
2023-09-15 17:00:37 -0500

    documentation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
-#include <cstdio>
+#include <iostream>
+#include <unistd.h>
+
+/*
+*	gnuish - GNU island shell
+*	
+*	gnuish displays the current working directory in the shell prompt:
+* 
+*		~ @ 
+*		/usr/ @
+*		/mnt/.../repos @
+* 
+*	Commands
+* 
+*		<command> [<args>...]	 Run command or program with optional arguments.
+* 
+*		r [<n>]		Execute the nth last line. 
+*					The line will be placed in history, not the `r` invocation.
+*
+*	Built-ins:
+*
+*		exit		Exit the shell.
+* 
+*		hist		Display up to 10 last lines entered, numbered.
+*
+*		----
+* 
+*		echo		Write to stdout.
+* 
+*		kill		Send a signal to a process.
+*
+*		help		Display this help page.
+* 
+*	Globbing
+* 
+*		TODO: globbing
+*/
 
 int main()
 {
-    printf("hello from %s!\n", "shell_experiment");
-    return 0;
+	std::cout << "One\n";
+
+	fork();
+
+	std::cout << "Two\n";
+
+	return 0;
 }
