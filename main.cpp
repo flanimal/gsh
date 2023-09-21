60c02053624e05753d7bfc4fc8394b6713aa229b
Nick Smith <nbsmithcc@gmail.com>
2023-09-21 03:04:55 -0500

    starting point with fork bomb

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <unistd.h>
+#include <cstring>
+#include <sys/wait.h>
 
 /*
 *	gnuish - GNU island shell
@@ -16,6 +18,7 @@
 * 
 *		r [<n>]		Execute the nth last line. 
 *					The line will be placed in history, not the `r` invocation.
+*					The line in question will be echoed to the screen before being executed.
 *
 *	Built-ins:
 *
@@ -34,15 +37,86 @@
 *	Globbing
 * 
 *		TODO: globbing
+* 
+*	***
+* 
+*	See the "Readme" file within this directory.
+*	
+*	Also see the Makefile, which:
+*		1. Compiles all source code.
+*		2. Cleans up the directory.
+*		3. Performs otehr tasks as required.
 */
 
-int main()
+namespace gnuish
 {
-	std::cout << "One\n";
+	void exit_shell()
+	{
+		exit(0);
+	}
 
-	fork();
+	void echo()
+	{
 
-	std::cout << "Two\n";
+	}
 
+	void kill()
+	{
+
+	}
+
+	void help()
+	{
+
+	}
+}
+
+int main(int argc, char* argv[], char* envp[])
+{
+	// Upon typing the name of this program in Bash, a child process will be forked
+	// and the process image replaced with this program. So we need not do that ourselves.
+	
+	// ***
+	
+	/* Main loop. */
+	char line[255];
+	char** args = new char* [255];
+
+	for (std::size_t nbytes; (nbytes = read(STDIN_FILENO, &line, 255)) > 0; )
+	{
+		// We must handle user input.
+		// We look at [?] for keypresses.
+		// Upon receipt of a carriage return (CR) character, we fork a child process, 
+		
+		write(STDOUT_FILENO, &line, nbytes);
+		
+		// and in that process we take the preceding line of input and pass it to `execve(2)` or `exec(3)`. 
+		
+		char* pathname = strtok(line, " \n"); // Must remember to include newline as delimiter!
+
+		args[0] = pathname;
+
+		int argNum = 1;
+		while ((args[argNum++] = strtok(nullptr, " ")))
+			;
+
+		args[argNum] = nullptr;
+
+		pid_t cmdChild = fork();
+
+		if (cmdChild)
+			execve(pathname, args, envp);
+		else
+			waitpid(cmdChild, nullptr, 0);
+		
+		// !!! I seem to have accidentally created a fork bomb or something.
+
+		//	The line of input will then be pushed onto the history stack.
+		//	If this will result in more than 10 lines on the stack, the oldest one will be popped.
+		//	*** If we use a stack, how will we execute the "n"th last command without popping?
+
+	}
+
+	printf("Process ending\n");
 	return 0;
 }
