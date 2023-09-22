ea5fceaced6f0bc7784b6d1c2e5d9b4ecda9304a
Nick Smith <nbsmithcc@gmail.com>
2023-09-22 18:24:24 -0500

    add project directory, .clang-format

--- shell-experiment/gnuish.c
+++ shell-experiment/gnuish.c
@@ -8,43 +8,43 @@
 
 /*
 *	gnuish - GNU island shell
-*	
+*
 *	gnuish displays the current working directory in the shell prompt:
-* 
-*		~ @ 
+*
+*		~ @
 *		/usr/ @
 *		/mnt/.../repos @
-* 
+*
 *	Commands
-* 
+*
 *		<command> [<args>...]	 Run command or program with optional arguments.
-* 
-*		r [<n>]		Execute the nth last line. 
+*
+*		r [<n>]		Execute the nth last line.
 *					The line will be placed in history, not the `r` invocation.
 *					The line in question will be echoed to the screen before being executed.
 *
 *	Built-ins:
 *
 *		exit		Exit the shell.
-* 
+*
 *		hist		Display up to 10 last lines entered, numbered.
 *
 *		----
-* 
+*
 *		echo		Write to stdout.
-* 
+*
 *		kill		Send a signal to a process.
 *
 *		help		Display this help page.
-* 
+*
 *	Globbing
-* 
+*
 *		TODO: globbing
-* 
+*
 *	***
-* 
+*
 *	See the "Readme" file within this directory.
-*	
+*
 *	Also see the Makefile, which:
 *		1. Compiles all source code.
 *		2. Cleans up the directory.
@@ -59,23 +59,25 @@ static void gnuish_put_prompt()
 	write(STDOUT_FILENO, GNUISH_PROMPT, sizeof(GNUISH_PROMPT));
 }
 
-static void gnuish_parse_line(const char* line, char** out_args)
+static void gnuish_parse_line(const char *line, char **out_args)
 {
-	char* lineTmp = malloc(strlen(line));
+	char *lineTmp = malloc(strlen(line));
 	strcpy(lineTmp, line);
 
 	out_args[0] = strtok(lineTmp, " \n");
 
-	for (int arg_n = 1; (out_args[arg_n++] = strtok(NULL, " ")) && arg_n <= GNUISH_MAX_ARGS; )
+	for (int arg_n = 1; (out_args[arg_n++] = strtok(NULL, " ")) &&
+			    arg_n <= GNUISH_MAX_ARGS;)
 		;
 
 	free(lineTmp);
 }
 
-static void gnuish_add_hist(struct gnuish_state* sh_state, char* line)
+static void gnuish_add_hist(struct gnuish_state *sh_state, char *line)
 {
 	// Allocate memory for the node.
-	struct gnuish_past_cmd* last_cmd = malloc(sizeof(struct gnuish_past_cmd));
+	struct gnuish_past_cmd *last_cmd =
+		malloc(sizeof(struct gnuish_past_cmd));
 
 	last_cmd->line = malloc(strlen(line));
 
@@ -85,79 +87,71 @@ static void gnuish_add_hist(struct gnuish_state* sh_state, char* line)
 	sh_state->cmd_history = last_cmd;
 }
 
-ssize_t gnuish_read_line(struct gnuish_state* sh_state, char* out_line)
+ssize_t gnuish_read_line(struct gnuish_state *sh_state, char *out_line)
 {
 	gnuish_put_prompt();
 
-	ssize_t nbytes = read(STDIN_FILENO, out_line, (size_t)sh_state->max_input);
-	out_line[nbytes] = '\0'; // Must remember to include newline as delimiter!
+	ssize_t nbytes =
+		read(STDIN_FILENO, out_line, (size_t)sh_state->max_input);
+	out_line[nbytes] =
+		'\0'; // Must remember to include newline as delimiter!
 
 	if (nbytes > 0)
-		write(STDOUT_FILENO, &out_line, (size_t)nbytes); // Echo line of input.
+		// Echo line of input.
+		write(STDOUT_FILENO, &out_line, (size_t)nbytes); 
 
 	gnuish_add_hist(sh_state, out_line);
 
 	return nbytes;
 }
 
-void gnuish_chdir(struct gnuish_state* sh_state, const char* pathname)
+void gnuish_chdir(struct gnuish_state *sh_state, const char *pathname)
 {
 	chdir(pathname);
 	sh_state->max_input = pathconf(pathname, _PC_MAX_INPUT);
 }
 
-void gnuish_recall(struct gnuish_state* sh_state, int n)
+void gnuish_recall(struct gnuish_state *sh_state, int n)
 {
-	struct gnuish_past_cmd* cmd_it = sh_state->cmd_history;
+	struct gnuish_past_cmd *cmd_it = sh_state->cmd_history;
 
-	while (cmd_it->next && n-- > 0)
-	{
+	while (cmd_it->next && n-- > 0) {
 		cmd_it = cmd_it->next;
 	}
 
 	gnuish_run_cmd(sh_state, cmd_it->line);
 }
 
-void gnuish_run_cmd(struct gnuish_state* sh_state, const char* line)
+void gnuish_run_cmd(struct gnuish_state *sh_state, const char *line)
 {
-	char** args = malloc(sizeof(char*) * GNUISH_MAX_ARGS);
+	char **args = malloc(sizeof(char *) * GNUISH_MAX_ARGS);
 
 	gnuish_parse_line(line, args);
-	char* pathname = args[0];
+	char *pathname = args[0];
 
-	if (strcmp(pathname, "cd") == 0)
-	{
+	if (strcmp(pathname, "cd") == 0) {
 		gnuish_chdir(sh_state, args[1]);
-	}
-	else if (strcmp(pathname, "r") == 0)
-	{
+	} else if (strcmp(pathname, "r") == 0) {
 		gnuish_recall(sh_state, atoi(args[1]));
-	}
-	else if (strcmp(pathname, "exit") == 0)
-	{
+	} else if (strcmp(pathname, "exit") == 0) {
 		gnuish_exit(sh_state);
-	}
-	else
-	{
+	} else {
 		gnuish_exec(sh_state, args);
 	}
 }
 
-void gnuish_exec(struct gnuish_state* sh_state, char** args)
+void gnuish_exec(struct gnuish_state *sh_state, char **args)
 {
 	pid_t cmdPid = fork();
 
-	if (cmdPid == 0)
-	{
+	if (cmdPid == 0) {
 		execve(args[0], args, sh_state->env);
-	}
-	else
-	{
+	} else {
 		waitpid(cmdPid, NULL, 0);
 	}
 }
 
-void gnuish_exit(struct gnuish_state* sh_state)
+void gnuish_exit(struct gnuish_state *sh_state)
 {
 	exit(0);
 }
