2fcd9242e08d707e890bf08322d79ee3a8f39a46
Nick Smith <nbsmithcc@gmail.com>
2023-09-22 16:55:17 -0500

    clean up main

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <unistd.h>
 #include <limits.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "gnuish.h"
 
 int main(int argc, char* argv[], char* envp[])
 {
-	char line[_POSIX_MAX_INPUT];
-	//char** args = new char* [255];
+	struct gnuish_state sh_state;
+	sh_state.env = envp; // TODO: We will probably need to copy the original environment.
 
-	for (ssize_t nbytes; (nbytes = gnuish_read_line(cmdHistory, line)) > 0; )
+	char* line = malloc(sh_state.max_input);
+
+	/* Main loop. */
+	for (ssize_t nbytes; (nbytes = gnuish_read_line(&sh_state, line)) > 0; )
 	{
-		gnuish_run_cmd(cmdHistory, args, line, envp);
+		gnuish_run_cmd(&sh_state, line);
 	}
 
 	printf("Process ending\n");
